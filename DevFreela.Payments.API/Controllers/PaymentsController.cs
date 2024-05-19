bd1cec511f2de236b87da6803f0e5d580f3c0500
vitor <vitorsalves0812@gmail.com>
2024-05-19 18:56:15 -0300

    initial commit

---
+++ DevFreela.Payments.API/Controllers/PaymentsController.cs
@@ -0,0 +1,31 @@
+﻿using DevFreela.Payments.API.Models;
+using DevFreela.Payments.API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.Payments.API.Controllers
+{
+    [Route("api/payments")]
+    [ApiController]
+    public class PaymentsController : ControllerBase
+    {
+        private readonly IPaymentService _paymentService;
+
+        public PaymentsController(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(PaymentInfoInputModel paymentInfo)
+        {
+            var result = await _paymentService.Process(paymentInfo);
+
+            if (!result)
+                return BadRequest();
+
+            return NoContent();
+
+        }
+    }
+}
